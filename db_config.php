cde883cc2d5629449f6fa128490f21c953a38035
Nurbek Ismoilov <nuronbeck@gmail.com>
2019-10-20 02:09:29 +0500

    Update db config heroku

--- db_config.php
+++ db_config.php
@@ -1,5 +1,5 @@
 <?php 
-	$connection = mysqli_connect("127.0.0.1", "root", "", "marathonBet_dump");
+	$connection = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b02a5ef2a06370", "90106657", "heroku_e2596b9a4c8c9fd");
 
 	if ($connection == false)
 	{
